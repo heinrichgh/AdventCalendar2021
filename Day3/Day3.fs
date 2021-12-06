a9d3f3b93e732a910366fa7faa2973d8451906c0
Heinrich G Henning <heinrichgh@gmail.com>
2021-12-06 19:30:06 +0200

    Day3 part 1 done

--- Day3/Day3.fs
+++ Day3/Day3.fs
@@ -1,2 +1,32 @@
 ﻿module AdventCalendar2021.Day3
 
+open System
+
+let diagnosticReport = System.IO.File.ReadLines("Day3/input.txt")
+                    |> Seq.map (fun elem -> elem |> Seq.map System.Globalization.CharUnicodeInfo.GetDigitValue |> Seq.toArray)
+                    |> Seq.toArray
+                            
+// count 1's into array, compare with list length to see if majority or not
+let empty:int[] = Array.zeroCreate diagnosticReport[0].Length
+
+let addTogether totalsList binaries = Array.zip totalsList binaries
+                                    |> Array.map (fun (a,b) -> a + b)
+
+let totalOnes = Array.fold addTogether empty diagnosticReport
+
+let isMajority count = count > (diagnosticReport.Length / 2)
+let convertBinaryToInteger (string:string) = Convert.ToInt32(string, 2)
+
+let majorityBits = Array.map isMajority totalOnes
+
+let gammaRate = majorityBits
+                |> Array.map (fun isMajority -> if isMajority then "1" else "0") 
+                |> String.concat ""
+                |> convertBinaryToInteger
+
+let epsilonRate = majorityBits
+                |> Array.map (fun isMajority -> if isMajority then "0" else "1") 
+                |> String.concat ""
+                |> convertBinaryToInteger                 
+
+let part1 = gammaRate * epsilonRate;
--- Program.fs
+++ Program.fs
@@ -4,4 +4,6 @@ open AdventCalendar2021
 //printfn "%A" Day1.part2
 
 //printfn "%A" Day2.part1     
-printfn "%A" Day2.part2      
+//printfn "%A" Day2.part2
+
+printfn "%A" Day3.part1      
