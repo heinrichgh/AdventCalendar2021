0d2751b8722de9bdb2c8511b947d76862f65aa7b
Heinrich G Henning <heinrichgh@gmail.com>
2021-12-07 08:52:38 +0200

    Day4 deel1 done

---
+++ Day4/Day4.fs
@@ -0,0 +1,66 @@
+﻿module AdventCalendar2021.Day4
+
+open System
+
+let input =
+    System.IO.File.ReadAllText("Day4/input.txt")
+
+let newLine = "\r\n"
+let sections = input.Split(newLine + newLine)
+
+let drawnNumbers =
+    sections[0].Split(",", StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map int
+
+let bingoBoards =
+    Array.skip 1 sections
+    |> Array.map
+        (fun line ->
+            line.Split([| newLine; " " |], StringSplitOptions.RemoveEmptyEntries)
+            |> Array.map (int >> Some))
+
+
+let rec markNumber number bingoBoard =
+    bingoBoard
+    |> Array.map (fun x -> if x = Some(number) then None else x)
+
+let rowsToColumns rows =
+    rows
+    |> Array.collect Array.indexed
+    |> Array.groupBy fst
+    |> Array.map (snd >> Array.map snd)
+    
+let checkAllMarked numbers =
+    let unmarked = numbers |> Array.choose id |> Array.length
+    unmarked = 0
+    
+
+let checkForWinner bingoBoard =
+    let rows = bingoBoard |> Array.chunkBySize 5
+    let finishedRows = rows |> Array.filter checkAllMarked
+    match finishedRows.Length with
+    | 0 ->     
+        let columns = rowsToColumns rows
+        let finishedColumns = columns |> Array.filter checkAllMarked
+        finishedColumns.Length <> 0
+    | _ -> true
+
+let calculateFinalScore (board: int option []) number =
+    let boardScore = board |> Array.choose id |> Array.sum
+    boardScore * number
+
+let rec playBingo (numbers: int []) bingoBoards =
+    let number = Array.head numbers
+
+    let boards =
+        bingoBoards |> Array.map (markNumber number)
+
+    let winnerBoard = boards |> Array.tryFind checkForWinner
+
+    match winnerBoard with
+    | Some (board) -> (board, number)
+    | None -> playBingo (Array.tail numbers) boards
+
+let (winningBoard, lastNumber) = playBingo drawnNumbers bingoBoards
+let sumOfUnmarkedNumbers = winningBoard |> Array.choose id |> Array.sum
+let part1 = sumOfUnmarkedNumbers * lastNumber
--- Program.fs
+++ Program.fs
@@ -7,4 +7,6 @@ open AdventCalendar2021
 //printfn "%A" Day2.part2
 
 //printfn "%A" Day3.part1      
-printfn "%A" Day3.part2      
+//printfn "%A" Day3.part2
+
+printfn "%A" Day4.part1      
